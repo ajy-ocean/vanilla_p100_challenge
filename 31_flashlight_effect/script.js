8380162a92c99c6ac8922bad8bd8d839e7ccd436
Ajay Rawat <93385633+ajy-ocean@users.noreply.github.com>
2025-11-18 09:46:34 +0530

    project 31 completed

---
+++ 31_flashlight_effect/script.js
@@ -0,0 +1,30 @@
+let mouseX = 0;
+let mouseY = 0;
+
+let flashLight = document.getElementById("flashlight");
+const isTouchDevice = () => {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+};
+
+function getMousePosition(e) {
+    mouseX  = !isTouchDevice() ? 
+    e.pageX : e.touches[0].pageX;
+    
+    mouseY = !isTouchDevice() ? 
+    e.pageY : e.touches[0].pageY;
+
+    flashLight.style.setProperty("--Xpos", mouseX + "px");
+    flashLight.style.setProperty("--Ypos", mouseY + "px");
+}
+
+document.addEventListener("mousemove", 
+    getMousePosition);
+
+document.addEventListener("touchmove",
+    getMousePosition);
