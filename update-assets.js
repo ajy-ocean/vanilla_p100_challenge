71b2ae60a4dd816f0405ef2250948f3d12f8a12a
Ajay Rawat <93385633+ajy-ocean@users.noreply.github.com>
2025-12-09 20:58:17 +0530

    Fix asset URLs to use master branch

--- update-assets.js
+++ update-assets.js
@@ -1,28 +1,19 @@
 // update-assets.js
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const REPO_BASE =
-    "https://raw.githubusercontent.com/ajy-ocean/vanilla_p100_challenge/main";
-const folders = fs
-    .readdirSync("./")
-    .filter((f) => fs.statSync(f).isDirectory() && /^\d+/.test(f));
+const REPO_BASE = 'https://raw.githubusercontent.com/ajy-ocean/vanilla_p100_challenge/master';
+const folders = fs.readdirSync('./').filter(f => fs.statSync(f).isDirectory() && /^\d+/.test(f));
 
-folders.forEach((folder) => {
-    const htmlPath = path.join(folder, "index.html");
+folders.forEach(folder => {
+    const htmlPath = path.join(folder, 'index.html');
     if (fs.existsSync(htmlPath)) {
-        let html = fs.readFileSync(htmlPath, "utf8");
+        let html = fs.readFileSync(htmlPath, 'utf8');
         // Replace images
-        html = html.replace(
-            /src="(?:\.\/)?images\/([^"]+)"/g,
-            `src="${REPO_BASE}/${folder}/images/$1"`
-        );
+        html = html.replace(/src="(?:\.\/)?images\/([^"]+)"/g, `src="${REPO_BASE}/${folder}/images/$1"`);
         // Replace videos
-        html = html.replace(
-            /src="(?:\.\/)?videos\/([^"]+)"/g,
-            `src="${REPO_BASE}/${folder}/videos/$1"`
-        );
+        html = html.replace(/src="(?:\.\/)?videos\/([^"]+)"/g, `src="${REPO_BASE}/${folder}/videos/$1"`);
         fs.writeFileSync(htmlPath, html);
     }
 });
-console.log("Updated all projects!");
+console.log('Updated all projects!');
